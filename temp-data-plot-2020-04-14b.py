0de5f08bdda9dfd0409b2ad361e0fcdf7e543fcc
chibaf <chibaf@mac.com>
2020-04-22 18:50:56 +0900

    Add files via upload

--- temp-data-plot-2020-04-14b.py
+++ temp-data-plot-2020-04-14b.py
@@ -15,17 +15,12 @@ b=[list(x) for x in list(zip(*a))]; # transpose matrix
 
 time=b[0];x1=b[1];x2=b[2];x3=b[3];x4=b[4];x5=b[5];x6=b[6];x7=b[7];  # time, second Tc, third Tc
 
-c1,c2,c3,c4 = "blue","green","red","black"      # 各プロットの色
-l1,l2,l3,l4,l5,l6,l7 = "1","2","3","4","5","6","7"   # 各ラベル
+l1,l2,l3,l4,l5,l6,l7 = "1","2","3","4","5","6","7"   # each lable of curve
 fig, ax = plt.subplots()
 
-ax.set_xlabel('time')  # x軸ラベル
-ax.set_ylabel('temp')  # y軸ラベル
-ax.set_title(r'pipe temps') # グラフタイトル
-#ax.set_aspect('equal') # スケールを揃える
-#ax.grid()            # 罫線
-#ax.set_xlim([-10, 10]) # x方向の描画範囲を指定
-ax.set_ylim([0, 900])    # y方向の描画範囲を指定
+ax.set_xlabel('time')  # x axis labele
+ax.set_ylabel('temp')  # y axis labele
+ax.set_title(r'pipe temps') # titlerange of y direction
 #ax.plot(phi, y1, color=c1, label=l1)
 #ax.plot(phi, y2, color=c2, label=l2)
 #ax.plot(phi, y3, color=c3, label=l3)
@@ -36,7 +31,7 @@ ax.plot(x4, label=l4)
 ax.plot(x5, label=l5)
 ax.plot(x6, label=l6)
 ax.plot(x7, label=l7)
-ax.legend(loc=0)    # 凡例
-fig.tight_layout()  # レイアウトの設定
-# plt.savefig('hoge.png') # 画像の保存
+ax.legend(loc=0)    # legend of curves
+fig.tight_layout()  # layout setting
+plt.savefig('temps.png') # save plot
 plt.show()
